30df7d087e31d260af5086903fd98c6320dd4220
Pablo Garcia <yinxro@gmail.com>
2016-09-09 11:41:09 +0200

    Removed config files

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=etsiinfbot',
-    'username' => 'etsiinfbot',
-    'password' => 'etsiinfbotpass',
-    'charset' => 'utf8',
-];
