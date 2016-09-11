4f914d6f1bc0d00de7074a2d447c017ab8128159
svg153 <svg153@gmail.com>
2016-09-11 17:36:42 +0200

    Added new admins

--- config/params.php
+++ config/params.php
@@ -3,6 +3,8 @@
 return [
     'name' => 'ETSIINF Bot',
     'admins' => [
-        'Fril' => 4103888
+        'Fril' => 4103888,
+        'diegofpb' => 85785,
+        'Svg153' => 5341682
     ]
 ];
