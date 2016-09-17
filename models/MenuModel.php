a9f60ec6617902c2d165e6e2c62518be5584a619
Pablo Garcia <yinxro@gmail.com>
2016-09-17 21:00:15 +0200

    Initial files

---
+++ models/MenuModel.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: frildoren
+ * Date: 17/09/16
+ * Time: 20:33
+ */
+class MenuModel
+{
+
+}
---
+++ models/repositories/MenuRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frildoren
+ * Date: 17/09/16
+ * Time: 20:08
+ */
+
+namespace app\models\repositories;
+
+
+class MenuRepository
+{
+
+}
