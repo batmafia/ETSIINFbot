6c735ec94f5e8b39cbb92e30ff7c3772657c3796
Pablo Garcia <yinxro@gmail.com>
2016-09-19 21:27:38 +0200

    last check?

--- controllers/WebHookController.php
+++ controllers/WebHookController.php
@@ -17,6 +17,7 @@ class WebHookController extends Controller
 
     public function actionDeploy()
     {
+        
         $payload = json_decode(file_get_contents('php://input'));
         if($payload->ref === "refs/heads/master") {
 
