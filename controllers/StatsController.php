e444bf452a453cfb561cd7e07c6e3c2e5e6e2d6d
Pablo Garcia <yinxro@gmail.com>
2016-10-11 18:26:30 +0200

    Implemented controller and view for charts

--- controllers/StatsController.php
+++ controllers/StatsController.php
@@ -9,7 +9,26 @@
 namespace app\controllers;
 
 
-class StatsController
+use yii\base\Controller;
+
+class StatsController extends Controller
 {
+
+    public function actionIndex()
+    {
+        $result = \Yii::$app->getDb()->createCommand(
+            "SELECT DATE(date) as day, COUNT(*) as requests, COUNT(DISTINCT user_id) as users FROM message WHERE DATE(date) > NOW()-INTERVAL 30 DAY AND LEFT(text, 1) = '/' GROUP BY day;"
+        )->queryAll();
+
+        $data = [];
+        foreach ($result as $r)
+        {
+            $data['days'][] = explode("-", $r['day'])[2];
+            $data['requests'][] = intval($r['requests']);
+            $data['users'][] = intval($r['users']);
+        }
+
+        return $this->render('index', $data);
+    }
 
 }
--- views/stats/index.php
+++ views/stats/index.php
@@ -1,7 +1,20 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: frildoren
- * Date: 11/10/16
- * Time: 17:23
+ * @var $this \yii\base\View
  */
+
+echo \miloschuman\highcharts\Highcharts::widget([
+    'options' => [
+        'title' => ['text' => 'Uso del mes'],
+        'xAxis' => [
+            'categories' => $days
+        ],
+        'yAxis' => [
+            'title' => ['text' => 'Unidades'],
+        ],
+        'series' => [
+            ['name' => 'Comandos', 'type'=>'column', 'data' => $requests],
+            ['name' => 'Usuarios únicos', 'data' => $users]
+        ]
+    ]
+]);
