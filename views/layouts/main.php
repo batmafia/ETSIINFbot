6bc176fac4dddf1fe7dc6231b8cb75b3fc25fb9b
Pablo Garcia <yinxro@gmail.com>
2016-10-12 12:02:37 +0200

    Removed unused menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,21 +36,7 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-            ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post', ['class' => 'navbar-form'])
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+            ['label' => 'Stats', 'url' => ['/stats/index']],
         ],
     ]);
     NavBar::end();
