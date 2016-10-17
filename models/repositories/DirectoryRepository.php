a189f624ce81a0012727f4829b46efc630a01e0b
Diego Fernández <diegofpb93@gmail.com>
2016-10-17 18:16:16 +0200

    Modified Directory Command (not finished) and finished repository.

--- models/repositories/DirectoryRepository.php
+++ models/repositories/DirectoryRepository.php
@@ -17,21 +17,35 @@ class DirectoryRepository
 
     public static function getDirectoryInfo($text)
     {
+
         $request = Request::get("http://www.etsiinf.upm.es/apps/personal/v2/?texto=$text")
             ->expects(Mime::JSON)->send();
         if (!$request->hasErrors()) {
 
-            print_r($request);
-
-            $dirObj = new DirectoryResponse();
             $data = \GuzzleHttp\json_decode($request->raw_body, true);
-            $dirObj->setAttributes($data);
 
-            if ($dirObj->validate()) {
-                return $dirObj;
-            } else {
-                print_r($dirObj->getErrors());
+            $availablePersonal = [];
+
+            foreach ($data as $directorioFI){
+                if ($directorioFI !== null){
+
+                    foreach ($directorioFI as $personal){
+
+                        $person = new DirectoryResponse();
+                        $person->setAttributes($personal);
+
+                        if ($person->validate()) {
+                            $availablePersonal[]=$person;
+                        } else {
+                            print_r($personal->getErrors());
+                        }
+                    }
+                }
+
+                return $availablePersonal;
             }
+
+
         } else {
             throw new Exception("Repository exception");
         }
