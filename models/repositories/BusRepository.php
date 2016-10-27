da1922d32fe5c7a8655ac868049ba5b710837291
svg153 <svg153@gmail.com>
2016-10-27 15:32:13 +0200

    Created getFullTimeBuses to take the autobuses_v2.json from http://www.etsiinf.upm.es/apps/autobuses/v2/

--- models/repositories/BusRepository.php
+++ models/repositories/BusRepository.php
@@ -3,6 +3,7 @@
 namespace app\models\repositories;
 
 use app\models\bus\BusStop;
+use app\models\bus\;
 use Httpful\Mime;
 use Httpful\Request;
 use yii\base\Exception;
@@ -30,6 +31,34 @@ class BusRepository
             else
             {
                 print_r($busObj->getErrors());
+            }
+        }
+        else
+        {
+            throw new Exception("Repository exception");
+        }
+
+    }
+
+    /**
+     * @return
+     */
+    public static function getFullTimeBuses()
+    {
+        $request = Request::get("http://www.etsiinf.upm.es/apps/autobuses/v2/")->expects(Mime::JSON)->send();
+        if(!$request->hasErrors())
+        {
+            $FullTimeBusesObj = new FullTimeBusesBusLines();
+            $data = \GuzzleHttp\json_decode($request->raw_body, true);
+            $FullTimeBusesObj->setAttributes($data);
+
+            if($FullTimeBusesObj->validate())
+            {
+                return $FullTimeBusesObj;
+            }
+            else
+            {
+                print_r($FullTimeBusesObj->getErrors());
             }
         }
         else
