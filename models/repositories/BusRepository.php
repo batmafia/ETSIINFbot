09bf3c3ce59673eac1c1aac48b7fb935d1c4c459
svg153 <svg153@gmail.com>
2016-10-27 21:05:54 +0200

    Create function to get FullTimeBuses by opts

--- models/repositories/BusRepository.php
+++ models/repositories/BusRepository.php
@@ -58,7 +58,7 @@ class BusRepository
                }
 
             }
-            echo $availableLines['591']['periodos']['lectivo']['horarios']['Aluche >> ETSIINF']['listadoHoras']['12345']['horas'][0];
+
         }
         else
         {
@@ -67,34 +67,66 @@ class BusRepository
         return $availableLines;
     }
 
-    public static function getFullTimeBusesOpts($idLine,$origin)
+    public static function getFullTimeBusesOpts($idLine, $origin)
     {
-        $request = Request::get("http://www.etsiinf.upm.es/apps/autobuses/v2/")->expects(Mime::JSON)->send();
-        if (!$request->hasErrors()) {
-            $data = \GuzzleHttp\json_decode($request->raw_body, true);
+        $availableLines = $this->getFullTimeBuses();
 
-            $availableLines=[];
-            foreach ($data as $key => $line)
-            {
-                foreach ($line as $k => $lineObject){
-                    $myLine = new fullTimeBuses\FullTimeBusesLine();
-                    $myLine->setAttributes($lineObject);
 
-                    if ($myLine->validate()) {
-                        $availableLines[$lineObject['idLinea']]=$myLine;
-                    } else {
-                        print_r($myLine->getErrors());
-                    }
-                }
-
-            }
-            echo $availableLines['591']['periodos']['lectivo']['horarios']['Aluche >> ETSIINF']['listadoHoras']['12345']['horas'][0];
+        $dayType = "";
+        if ($idLine==='591' || $idLine==='865') {
+            $dayType = 'lectivo';
         }
-        else
+        if ($idLine==='571') {
+            $dayType = 'laboral';
+        }
+        if ($idLine==='573') {
+            $dayType = 'laborsabado';
+        }
+
+
+        $dest = "";
+        if ($idLine==='591' && $origin==='Madrid') {
+            $dest = 'Aluche >> ETSIINF';
+        }
+        if ($idLine==='591' && $origin==='ETSIINF') {
+            $dest = 'ETSIINF >> Aluche';
+        }
+        if ($idLine==='865' && $origin==='Madrid') {
+            $dest = 'C.Universitaria >> ETSIINF';
+        }
+        if ($idLine==='865' && $origin==='ETSIINF') {
+            $dest = 'ETSIINF >> C.Universitaria';
+        }
+        if ($idLine==='571' && $origin==='Madrid') {
+            $dest = 'Aluche >> Boadilla';
+        }
+        if ($idLine==='571' && $origin==='ETSIINF') {
+            $dest = 'Boadilla >> Aluche';
+        }
+        if ($idLine==='573' && $origin==='Madrid') {
+            $dest = 'Moncloa >> Boadilla';
+        }
+        if ($idLine==='573' && $origin==='ETSIINF') {
+            $dest = 'Boadilla >> Moncloa';
+        }
+
+        $dw = idate("w", $timestamp);
+        $dayWeekNumber = ($dw===0)?7:$dw;
+
+        $hours = [];
+        foreach ($availableLines[$idLine]['periodos'][$dayType]['horarios'][$dest]['listadoHoras'] as $key => $value)
         {
-            throw new Exception("Repository exception");
+            if (strpos($key, $dayWeekNumber) !== FALSE)
+            {
+                $hours = array_merge( $hours, $value['horas']);
+            }
         }
-        return $availableLines;
+
+        print_r($hours);
+        //echo $availableLines[$idLine]['periodos'][$dayType]['horarios'][$dest]['listadoHoras']['12345']['horas'][0];
+
+        return $hours;
+
     }
 
 
