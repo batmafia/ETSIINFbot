32c919ab290c53f2eb496661d32e643cbafde829
svg153 <svg153@gmail.com>
2016-10-28 02:18:57 +0200

    fix time in UTC to GMT

--- models/repositories/BusRepository.php
+++ models/repositories/BusRepository.php
@@ -7,6 +7,9 @@ use app\models\bus\fullTimeBuses;
 use Httpful\Mime;
 use Httpful\Request;
 use yii\base\Exception;
+use \DateTime;
+use \DateTimeZone;
+
 
 class BusRepository
 {
@@ -111,23 +114,38 @@ class BusRepository
             $dest = 'Boadilla >> Moncloa';
         }
 
-        $dw = idate("w", time());
+        $nowTimeSTR = self::myDateFormat("D M j G:i:s T Y", false, 'Europe/Madrid'); // false for timeestamp
+        $nowTime = strtotime($nowTimeSTR);
+        $dw = idate("w", $nowTime);
         $dayWeekNumber = ($dw===0)?7:$dw;
 
         $hours = [];
-
         foreach ($availableLines[$idLine]['periodos'][$dayType]['horarios'][$dest]['listadoHoras'] as $key => $value)
         {
             if (strpos($key, "$dayWeekNumber") !== FALSE)
             {
-                $hours = array_merge( $hours, $value['horas']);
+                $hours = array_merge($hours, $value['horas']);
             }
         }
-
         //echo $availableLines[$idLine]['periodos'][$dayType]['horarios'][$dest]['listadoHoras']['12345']['horas'][0];
-
         return $hours;
+    }
 
+
+    /**
+     * http://php.net/manual/es/function.date.php
+     * @param  string  $format    [description]
+     * @param  boolean $timestamp [description]
+     * @param  boolean $timezone  [description]
+     * @return [type]             [description]
+     */
+    function myDateFormat($format="r", $timestamp=false, $timezone=false)
+    {
+        $userTimezone = new DateTimeZone(!empty($timezone) ? $timezone : 'GMT');
+        $gmtTimezone = new DateTimeZone('GMT');
+        $myDateTime = new DateTime(($timestamp!=false?date("r",(int)$timestamp):date("r")), $gmtTimezone);
+        $offset = $userTimezone->getOffset($myDateTime);
+        return date($format, ($timestamp!=false?(int)$timestamp:$myDateTime->format('U')) + $offset);
     }
 
 
