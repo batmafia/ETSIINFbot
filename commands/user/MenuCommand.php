118ab357f2a74d9f8a74f5c978452ee45336cd32
svg153 <svg153@gmail.com>
2019-02-19 00:49:06 +0100

    Improve msg

--- commands/user/MenuCommand.php
+++ commands/user/MenuCommand.php
@@ -73,7 +73,7 @@ class MenuCommand extends BaseUserCommand
         if($selectedMenu !== null)
         {
             $hbIcon = "\xF0\x9F\x8D\x94";
-            $dateTo = date("d/m/Y", $selectedMenu->validTo);
+            $dateTo = date("d/m/Y", $menus[$selectedMenu]->validTo);
             $cap = "Aquí tienes el menú hasta el $dateTo";
             $linkMenu = $menus[$selectedMenu]->link;
 
